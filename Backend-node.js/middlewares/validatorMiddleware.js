26124e9e824e79f9bc10e32540c8a3ad11186ab1
Ebram_Samuel <ebramsam2001@gmail.com>
2026-03-26 16:14:20 +0200

    Add files via upload

---
+++ Backend-node.js/middlewares/validatorMiddleware.js
@@ -0,0 +1,40 @@
+const productSchema = require('../validators/productValidation');
+const categorySchema = require("../validators/categoryValidation");
+
+const productValidation = () => {
+    return (request, response, next) => {
+        // Ensure productSchema is imported here
+        const { error } = productSchema.validate(request.body, { abortEarly: false });
+        
+        if (error) {
+            const errorMessage = error.details.map(detail => detail.message).join(', ');
+            const validationError = new Error(errorMessage);
+            validationError.statusCode = 400;
+            return next(validationError);
+        }
+        next();
+    };
+};
+
+const categoryValidation = () => {
+    return (request, response, next) => {
+        const { error } = categorySchema.validate(
+            request.body,
+            {
+                abortEarly: false
+            }
+        );
+        
+        if (error) {
+            const errorMessage = error.details.map(detail => detail.message)
+                                              .join(', ');
+            const validationError = new Error(errorMessage);
+            validationError.statusCode = 400;
+            return next(validationError);
+        }
+        
+        next();
+    };
+};
+
+module.exports = { productValidation, categoryValidation };
---
+++ Backend-node.js/routes/categoryRoutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const { protect, authorize } = require("../middlewares/authMiddleware");
+const { categoryValidation } = require("../middlewares/validatorMiddleware");
+const {
+    getAllCategories,
+    getCategoryById,
+    createCategory,
+    updateCategory,
+    deleteCategory,
+} = require("../controllers/categoryControllers");
+
+router.get("/", getAllCategories);
+router.get("/:id", getCategoryById);
+
+// Restricted to Admin
+router.post("/", protect, authorize('admin'), categoryValidation(), createCategory);
+router.put("/:id", protect, authorize('admin'), categoryValidation(), updateCategory);
+router.delete("/:id", protect, authorize('admin'), deleteCategory);
+
+module.exports = router;
